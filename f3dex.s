f67dd2bfe0871c4eeaac9cda4b4d2802d20e6a84
coco875 <59367621+coco875@users.noreply.github.com>
2023-10-28 23:42:17 +0200

    add all branch

--- f3dex.s
+++ f3dex.s
@@ -84,10 +84,12 @@ mfc0 $2, $4
 andi $2, $2, 128
 bne $2, $0, branch_0x10C0
 lh $21, 38($0)
+branch_0x10AC:
 bne $28, $27, 0x4001064
 lw $25, 0($27)
 j branch_0x10c8
 lh $31, 260($0)
+branch_0x10BC:
 lh $21, 182($0)
 branch_0x10C0:
 j branch_0x10F0
@@ -109,9 +111,10 @@ lw $19, 0($30)
 lh $18, 4($30)
 jal branch_0x113c
 lh $20, 6($30)
-jal 0x400112c
+jal branch_0x112C
 sll $0, $0, 0
 jr $21
+branch_0x110C:
 lw $11, 184($0)
 srl $12, $19, 22
 andi $12, $12, 60
@@ -120,53 +123,62 @@ add $13, $0, $12
 lw $12, 352($13)
 jr $31
 add $19, $19, $12
+branch_0x112C:
 mfc0 $11, $6
-bne $11, $0, 0x400112c
+bne $11, $0, branch_0x112C
 sll $0, $0, 0
 jr $31
 branch_0x113c:
 add $17, $0, $0
+branch_0x1140:
 mfc0 $11, $5
-bne $11, $0, 0x4001140
+bne $11, $0, branch_0x1140
 sll $0, $0, 0
 mtc0 $20, $0
-bgtz $17, 0x4001160
+bgtz $17, branch_0x1160
 mtc0 $19, $1
 jr $31
 mtc0 $18, $2
+branch_0x1160:
 jr $31
 mtc0 $18, $3
+branch_0x1168:
 add $21, $0, $31
 lw $19, 24($29)
 addi $18, $23, 62240
 lw $23, 68($29)
-blez $18, 0x40011e4
+blez $18, branch_0x11E4
 add $20, $19, $18
 sub $20, $23, $20
-bgez $20, 0x40011a8
+bgez $20, branch_0x11A8
+branch_0x1188:
 mfc0 $20, $11
 andi $20, $20, 1024
-bne $20, $0, 0x4001188
+bne $20, $0, branch_0x1188
+branch_0x1194:
 mfc0 $23, $10
 lw $19, 64($29)
-beq $23, $19, 0x4001194
+beq $23, $19, branch_0x1194
 sll $0, $0, 0
 mtc0 $19, $8
+branch_0x11A8:
 mfc0 $23, $10
 sub $20, $19, $23
-bgez $20, 0x40011c4
+bgez $20, branch_0x11C4
 add $20, $19, $18
 sub $20, $20, $23
-bgez $20, 0x40011a8
+bgez $20, branch_0x11A8
 sll $0, $0, 0
+branch_0x11C4:
 add $23, $19, $18
 addi $18, $18, 65535
 li $20, 3296
-jal 0x4001140
+jal branch_0x1140
 li $17, 1
-jal 0x400112c
+jal branch_0x112C
 sw $23, 24($29)
 mtc0 $23, $9
+branch_0x11E4:
 jr $21
 li $23, 3296
 andi $2, $2, 254
@@ -174,24 +186,27 @@ lh $2, 118($2)
 jr $2
 lbu $1, 65535($27)
 lh $24, 190($0)
+branch_0x1200:
 lh $31, 918($0)
+branch_0x1204:
 lbu $1, 65533($27)
 lbu $2, 65534($27)
 lbu $3, 65535($27)
+branch_0x1210:
 lhu $1, 796($1)
 lhu $2, 796($2)
 lhu $3, 796($3)
 jr $31
 addi $4, $1, 0
-jal 0x4001200
+jal branch_0x1200
 addi $24, $31, 0
 lbu $1, 65529($27)
 lbu $2, 65530($27)
 lbu $3, 65531($27)
 lh $31, 918($0)
-j 0x4001210
+j branch_0x1210
 lh $24, 190($0)
-jal 0x4001204
+jal branch_0x1204
 lbu $7, 65532($27)
 lhu $7, 796($7)
 lh $8, 26($1)
@@ -199,13 +214,15 @@ lh $10, 26($3)
 lh $9, 26($2)
 lh $11, 26($7)
 sub $6, $8, $10
-bgez $6, 0x4001270
+bgez $6, branch_0x1270
 sh $1, 3290($0)
 sub $6, $10, $8
+branch_0x1270:
 sub $12, $9, $11
-bgez $12, 0x4001280
+bgez $12, branch_0x1280
 sh $3, 3292($0)
 sub $12, $11, $9
+branch_0x1280:
 sub $6, $6, $12
 blez $6, branch_0x1294
 sh $7, 3294($0)
@@ -229,9 +246,9 @@ bgez $3, branch_0x109C
 addi $19, $19, 65472
 jal branch_0x113c
 li $18, 63
-jal 0x400112c
+jal branch_0x112C
 li $3, 1120
-j 0x4001644
+j branch_0x1644
 sw $19, 36($29)
 lbu $1, 65531($27)
 lhu $2, 65529($27)
@@ -248,9 +265,10 @@ sll $3, $3, 1
 or $2, $2, $3
 j branch_0x109C
 sh $2, 6($29)
-j 0x4001330
+j branch_0x1330
 addi $7, $29, 8
 addi $7, $29, 12
+branch_0x1330:
 lw $3, 0($7)
 lbu $5, 65531($27)
 lbu $6, 65530($27)
@@ -263,18 +281,19 @@ and $2, $2, $3
 or $3, $2, $24
 sw $3, 0($7)
 lw $25, 8($29)
-j 0x40013fc
+j branch_0x13FC
 lw $24, 12($29)
 andi $25, $25, 1023
 ori $2, $0, 28784
+branch_0x1370:
 lh $3, 1220($25)
 addi $25, $25, 40
-bne $25, $24, 0x4001370
+bne $25, $24, branch_0x1370
 and $2, $2, $3
 beq $2, $0, branch_0x109C
 lb $2, 0($29)
 addi $2, $2, 65532
-bltz $2, 0x40010bc
+bltz $2, branch_0x10BC
 addi $3, $2, 920
 lw $26, 0($3)
 sb $2, 0($29)
@@ -291,27 +310,28 @@ j branch_0x109C
 sw $2, 4($29)
 j branch_0x109C
 sh $24, 2($29)
-j 0x40010ac
+j branch_0x10AC
 sw $24, 65532($29)
-j 0x40013fc
+j branch_0x13FC
 lw $25, 65532($29)
 sra $2, $25, 24
 addi $2, $2, 3
-bltz $2, 0x40013fc
+bltz $2, branch_0x13FC
 addi $2, $2, 24
-jal 0x400110c
+jal branch_0x110C
 add $19, $24, $0
 add $24, $19, $0
+branch_0x13FC:
 sw $25, 0($23)
 sw $24, 4($23)
-jal 0x4001168
+jal branch_0x1168
 addi $23, $23, 8
 bgtz $2, branch_0x109C
 sll $0, $0, 0
-j 0x40010ac
+j branch_0x10AC
 andi $2, $2, 510
 lh $2, 196($2)
-jal 0x400112c
+jal branch_0x112C
 lbu $1, 65529($27)
 jr $2
 andi $6, $1, 15
@@ -321,11 +341,12 @@ lbu $9, 65530($27)
 ldv v2[0], 0(r22)
 ldv v2[8], 16(r22)
 srl $9, $9, 2
-jal 0x40016e8
+jal branch_0x16E8
 lhu $7, 796($1)
 llv v17[0], 20(r29)
-jal 0x4001710
+jal branch_0x1710
 llv v17[8], 20(r29)
+branch_0x145C:
 vmudn v28, v12, v2[0h]
 llv v18[0], 8(r22)
 vmadh v28, v8, v2[0h]
@@ -339,9 +360,11 @@ vmadh v28, v10, v2[2h]
 vmadn v28, v15, v31[1w]
 llv v18[8], 24(r22)
 vmadh v29, v11, v31[1w]
-bne $1, $0, 0x40017b4
+bne $1, $0, branch_0x17B4
 addi $22, $22, 32
+branch_0x1498:
 vmudm v18, v18, v17
+branch_0x149C:
 lsv v21[0], 118(r0)
 vmudn v20, v28, v21[0w]
 vmadh v21, v29, v21[0w]
@@ -391,7 +414,7 @@ ldv v29[8], 40(r0)
 vmadn v7, v31, v31[0w]
 vge v6, v6, v29[1q]
 sw $15, 16($7)
-beq $12, $0, 0x4001590
+beq $12, $0, branch_0x1590
 vlt v6, v6, v29[0q]
 lqv v3[0], 912(r0)
 vmudn v5, v5, v3[0w]
@@ -403,11 +426,12 @@ sbv v4[5], 19($7)
 sw $16, 24($7)
 sbv v4[13], 27($7)
 lw $16, 24($7)
+branch_0x1590:
 slv v18[0], 20($7)
 sdv v6[0], 24($7)
 ssv v7[4], 30($7)
 ssv v27[6], 32($7)
-bltz $9, 0x40015d4
+bltz $9, branch_0x15D4
 ssv v26[6], 34($7)
 sdv v21[8], 40($7)
 sdv v20[8], 48($7)
@@ -419,54 +443,63 @@ ssv v27[14], 72($7)
 ssv v26[14], 74($7)
 sw $8, 76($7)
 addi $7, $7, 80
-bgtz $9, 0x400145c
+bgtz $9, branch_0x145C
+branch_0x15D4:
 lh $8, 262($0)
 jr $8
 andi $8, $1, 1
 sbv v31[6], 28($29)
-bne $8, $0, 0x4001654
+bne $8, $0, branch_0x1654
 andi $7, $1, 2
 li $20, 992
 andi $8, $1, 4
-beq $8, $0, 0x4001620
+beq $8, $0, branch_0x1620
 lqv v26[0], 48(r22)
 lw $19, 36($29)
 lw $8, 76($29)
 li $17, 1
 addi $1, $19, 64
-beq $19, $8, 0x4001620
+beq $19, $8, branch_0x1620
 li $12, 63
-jal 0x4001140
+jal branch_0x1140
 sw $1, 36($29)
-jal 0x400112c
+jal branch_0x112C
+branch_0x1620:
 lqv v28[0], 16(r22)
-beq $7, $0, 0x4001660
+beq $7, $0, branch_0x1660
 lqv v27[0], 32(r22)
 sqv v26[0], 48($20)
 lqv v29[0], 0(r22)
 sqv v28[0], 16($20)
+branch_0x1638:
 li $3, 1120
 sqv v27[0], 32($20)
 sqv v29[0], 0($20)
+branch_0x1644:
 li $1, 992
 li $2, 1056
-j 0x4001684
+j branch_0x1684
 lh $31, 190($0)
+branch_0x1654:
 lqv v26[0], 48(r22)
-j 0x4001620
+j branch_0x1620
 li $20, 1056
+branch_0x1660:
 la $3, 3552
 addu $1, $0, $22
-jal 0x4001684
+jal branch_0x1684
 addu $2, $0, $20
 sqv v6[0], 48($20)
 sqv v5[0], 16($20)
 lqv v27[0], 0(r3)
-j 0x4001638
+j branch_0x1638
 lqv v29[0], 2016(r3)
+branch_0x1684:
 addi $19, $3, 16
+branch_0x1688:
 vmudh v5, v31, v31[0w]
 addi $18, $1, 8
+branch_0x1690:
 ldv v3[0], 0(r2)
 ldv v4[0], 32(r2)
 lqv v1[0], 0(r1)
@@ -479,16 +512,17 @@ vmadm v6, v3, v2[0h]
 addi $2, $2, 8
 vmadn v6, v4, v1[0h]
 vmadh v5, v3, v1[0h]
-bne $1, $18, 0x4001690
+bne $1, $18, branch_0x1690
 vmadn v6, v31, v31[0w]
 addi $2, $2, 65504
 addi $1, $1, 8
 sqv v5[0], 0($3)
 sqv v6[0], 32($3)
-bne $3, $19, 0x4001688
+bne $3, $19, branch_0x1688
 addi $3, $3, 16
 jr $31
 sll $0, $0, 0
+branch_0x16E8:
 li $8, 896
 lqv v3[0], 80(r0)
 lsv v19[0], 2(r29)
@@ -499,11 +533,13 @@ ldv v0[8], 0(r8)
 ldv v1[8], 8(r8)
 jr $31
 vmudh v0, v0, v3
+branch_0x1710:
 li $8, 1120
 ldv v11[0], 24(r8)
 ldv v11[8], 24(r8)
 ldv v15[0], 56(r8)
 ldv v15[8], 56(r8)
+branch_0x1724:
 ldv v8[0], 0(r8)
 ldv v9[0], 8(r8)
 ldv v10[0], 16(r8)
@@ -521,7 +557,7 @@ lqv v0[0], 0(r22)
 lh $5, 624($1)
 j branch_0x109C
 sqv v0[0], 0($5)
-bgtz $1, 0x4001790
+bgtz $1, branch_0x1790
 lb $2, 0($29)
 addi $4, $2, 65468
 bgtz $4, branch_0x109C
@@ -531,26 +567,30 @@ add $26, $26, $27
 sub $26, $26, $28
 sw $26, 0($3)
 sb $2, 0($29)
-jal 0x400110c
+branch_0x1790:
+jal branch_0x110C
 add $19, $24, $0
 add $26, $19, $0
 j branch_0x109C
 addi $28, $27, 0
 sll $0, $0, 0
+branch_0x17A8:
 ori $30, $0, 16
 beq $0, $0, branch_0x10F0
 lh $21, 256($0)
+branch_0x17B4:
 ori $30, $0, 24
 beq $0, $0, branch_0x10F0
 lh $21, 160($0)
 branch_0x17C0:
 ori $2, $0, 10240
 mtc0 $2, $4
+branch_0x17C8:
 lqv v31[0], 48(r0)
 lqv v30[0], 64(r0)
 lw $4, 4036($0)
 andi $4, $4, 1
-bne $4, $0, 0x40018b4
+bne $4, $0, branch_0x18B4
 sll $0, $0, 0
 lw $23, 40($1)
 lw $3, 44($1)
@@ -565,9 +605,10 @@ bgtz $23, branch_0x1824
 mfc0 $5, $10
 beq $5, $0, branch_0x1824
 sll $0, $0, 0
+branch_0x1814:
 beq $5, $4, branch_0x1824
 sll $0, $0, 0
-j 0x4001840
+j branch_0x1840
 move $3, $4
 branch_0x1824:
 mfc0 $4, $11
@@ -577,6 +618,7 @@ li $4, 1
 mtc0 $4, $11
 mtc0 $3, $8
 mtc0 $3, $9
+branch_0x1840:
 sw $3, 24($29)
 li $23, 3296
 lw $5, 16($1)
@@ -606,6 +648,7 @@ lw $2, 65528($0)
 sw $2, 264($0)
 j 0x4001058
 sll $0, $0, 0
+branch_0x18B4:
 jal branch_0x10EC
 li $30, 8
 lw $23, 3056($0)
@@ -657,6 +700,7 @@ sll $0, $0, 0
 sll $0, $0, 0
 sll $0, $0, 0
 sll $0, $0, 0
+branch_0x1980:
 sll $0, $0, 0
 sll $0, $0, 0
 sll $0, $0, 0
@@ -683,10 +727,10 @@ and $12, $11, $8
 or $11, $11, $8
 and $12, $12, $9
 andi $12, $12, 28784
-bne $12, $0, 0x4001fa4
+bne $12, $0, branch_0x1FA4
 or $11, $11, $9
 andi $11, $11, 17219
-bne $11, $0, 0x40017a8
+bne $11, $0, branch_0x17A8
 llv v13[0], 24(r1)
 llv v14[0], 24(r2)
 llv v15[0], 24(r3)
@@ -714,7 +758,7 @@ mfc2 $17, v16[0]
 sh $1, 3040($0)
 sh $2, 3042($0)
 and $15, $17, $15
-bltz $15, 0x4001fa0
+bltz $15, branch_0x1FA0
 sh $3, 3044($0)
 slt $7, $11, $10
 slt $12, $10, $9
@@ -732,11 +776,12 @@ lh $1, 3040($1)
 lbu $12, 885($7)
 llv v15[0], 24(r3)
 llv v14[0], 24(r2)
-blez $12, 0x4001ab8
+blez $12, branch_0x1AB8
 llv v13[0], 24(r1)
 vsubc v28, v31, v28
 vsub v29, v31, v29
-beq $17, $0, 0x4001fa4
+branch_0x1AB8:
+beq $17, $0, branch_0x1FA4
 vsub v4, v15, v14
 sll $14, $13, 19
 vsub v10, v14, v13
@@ -744,7 +789,7 @@ nor $14, $14, $0
 vsub v9, v15, v13
 or $14, $17, $14
 vmov v29[3], v29[0]
-bgez $14, 0x4001fa0
+bgez $14, branch_0x1FA0
 vmov v28[3], v28[0]
 ssv v15[2], 2($23)
 vmov v4[2], v10[0]
@@ -794,6 +839,7 @@ vmudl v1, v1, v31[4w]
 vmadm v0, v0, v31[4w]
 vmadn v1, v31, v31[0w]
 vand v16, v1, v30[1w]
+branch_0x1BA0:
 vmudm v12, v12, v31[4w]
 sw $2, 4($8)
 vmadn v13, v31, v31[0w]
@@ -826,7 +872,7 @@ ssv v9[4], 24($23)
 vmrg v20, v20, v6[2w]
 ssv v3[4], 26($23)
 addi $23, $23, 32
-blez $7, 0x4001ce8
+blez $7, branch_0x1CE8
 vmudl v20, v20, v30[5w]
 lw $14, 0($15)
 vmadm v19, v19, v30[5w]
@@ -876,10 +922,11 @@ vge v17, v9, v22
 vmrg v18, v10, v23
 vge v17, v17, v19
 vmrg v18, v18, v20
+branch_0x1CE8:
 slv v17[0], 64($8)
 slv v18[0], 72($8)
 andi $7, $5, 7
-blez $7, 0x4001f98
+blez $7, branch_0x1F98
 vxor v18, v31, v31
 luv v25[0], 16(r3)
 vadd v16, v18, v30[5w]
@@ -887,11 +934,12 @@ luv v15[0], 16(r1)
 vadd v24, v18, v30[5w]
 andi $7, $13, 512
 vadd v5, v18, v30[5w]
-bgtz $7, 0x4001d28
+bgtz $7, branch_0x1D28
 luv v23[0], 16(r2)
 luv v25[0], 16(r4)
 luv v15[0], 16(r4)
 luv v23[0], 16(r4)
+branch_0x1D28:
 vmudm v25, v25, v31[7w]
 vmudm v15, v15, v31[7w]
 vmudm v23, v23, v31[7w]
@@ -953,12 +1001,13 @@ vmadn v28, v31, v31[0w]
 vsubc v18, v16, v28
 vsub v17, v15, v6
 andi $7, $5, 4
-blez $7, 0x4001e30
+blez $7, branch_0x1E30
 andi $7, $5, 2
 addi $23, $23, 64
 sdv v17[0], 960($23)
 sdv v18[0], 976($23)
-blez $7, 0x4001f2c
+branch_0x1E30:
+blez $7, branch_0x1F2C
 andi $7, $5, 1
 li $16, 2048
 mtc2 $16, v19
@@ -1021,13 +1070,15 @@ sdv v24[8], 56($23)
 addi $23, $23, 64
 sdv v25[8], 992($23)
 sdv v26[8], 1008($23)
-blez $7, 0x4001f98
+branch_0x1F2C:
+blez $7, branch_0x1F98
 vmudn v14, v14, v30[4w]
 vmadh v13, v13, v30[4w]
 vmadn v14, v31, v31[0w]
 vmudn v16, v16, v30[4w]
 vmadh v15, v15, v30[4w]
 vmadn v16, v31, v31[0w]
+branch_0x1F48:
 ssv v13[14], 8($23)
 vmudn v10, v10, v30[4w]
 ssv v14[14], 10($23)
@@ -1048,9 +1099,12 @@ vsub v17, v15, v6
 addi $23, $23, 16
 ssv v17[14], 240($23)
 ssv v18[14], 242($23)
-jal 0x4001168
+branch_0x1F98:
+jal branch_0x1168
 addi $31, $24, 0
+branch_0x1FA0:
 li $31, 0
+branch_0x1FA4:
 jr $24
 sll $0, $0, 0
 sll $0, $0, 0
@@ -1076,25 +1130,27 @@ vmadn v26, v26, v25
 vmadh v27, v27, v25
 jr $31
 sll $0, $0, 0
-jal 0x400112c
+jal branch_0x112C
 li $27, 2464
 lw $25, 0($27)
 lw $24, 4($27)
 srl $1, $25, 29
 andi $1, $1, 6
 addi $27, $27, 8
-bgtz $1, 0x4002040
+bgtz $1, branch_0x2040
 andi $18, $25, 1023
 li $22, 2784
-jal 0x400110c
+jal branch_0x110C
 add $19, $24, $0
-beq $0, $0, 0x4002054
+beq $0, $0, branch_0x2054
 sh $31, 344($0)
+branch_0x2040:
 sll $0, $0, 0
 sll $0, $0, 0
 ori $30, $0, 24
-beq $0, $0, 0x4001980
+beq $0, $0, branch_0x1980
 lh $21, 160($0)
+branch_0x2054:
 sh $3, 3136($0)
 sh $2, 3138($0)
 sh $1, 3140($0)
@@ -1104,29 +1160,33 @@ ori $30, $0, 3136
 ori $6, $0, 12
 or $5, $30, $30
 xori $30, $30, 20
-beq $6, $0, 0x4002224
+branch_0x2078:
+beq $6, $0, branch_0x2224
 lh $11, 166($6)
 addi $6, $6, 65534
 move $17, $0
 or $18, $0, $0
 move $2, $5
-j 0x4001814
+j branch_0x1814
 addi $14, $30, 2
+branch_0x2098:
 and $8, $8, $11
-beq $8, $18, 0x40020d4
+beq $8, $18, branch_0x20D4
 addi $2, $2, 2
 or $20, $10, $0
 sh $10, 0($14)
 addi $14, $14, 2
+branch_0x20B0:
 lh $10, 0($2)
-bne $10, $0, 0x4002098
+bne $10, $0, branch_0x2098
 lh $8, 36($10)
 addi $8, $17, 65534
-bgtz $8, 0x40020b0
+bgtz $8, branch_0x20B0
 move $2, $5
-beq $8, $0, 0x4002078
+beq $8, $0, branch_0x2078
 sll $0, $0, 0
 jr $24
+branch_0x20D4:
 xor $18, $18, $11
 lh $8, 246($17)
 addi $17, $17, 2
@@ -1163,10 +1223,11 @@ vrcph v7[3], v29[3]
 vrcpl v3[3], v28[3]
 vrcph v7[3], v31[0]
 vmudn v3, v3, v31[2w]
-bgez $8, 0x4002174
+bgez $8, branch_0x2174
 vmadh v7, v7, v31[2w]
 vmudn v3, v3, v31[3w]
 vmadh v7, v7, v31[3w]
+branch_0x2174:
 veq v7, v7, v31[0w]
 vmrg v3, v3, v31[3w]
 vmudl v28, v28, v3[3w]
@@ -1206,11 +1267,12 @@ vmadm v18, v11, v8[3w]
 suv v18[0], 0($7)
 sdv v18[8], 8($7)
 ldv v18[0], 8(r7)
-jal 0x40016e8
+jal branch_0x16E8
 lw $15, 0($7)
 mfc2 $10, v13[0]
-j 0x400149c
+j branch_0x149C
 ori $9, $0, 1
+branch_0x2224:
 lh $8, 0($5)
 sh $5, 262($0)
 addi $30, $24, 0
@@ -1221,20 +1283,22 @@ lh $3, 180($0)
 lh $2, 2($8)
 lh $1, 4($8)
 addi $8, $8, 2
-bne $1, $0, 0x4002284
+bne $1, $0, branch_0x2284
 sh $8, 262($0)
 jalr $30, $31
 ori $30, $0, 16
-beq $0, $0, 0x4001ba0
+beq $0, $0, branch_0x1BA0
 lh $21, 256($0)
 lw $1, 300($0)
 sw $15, 0($7)
 sw $16, 4($7)
-bltz $1, 0x40022fc
+bltz $1, branch_0x22FC
 lpv v4[0], 0(r7)
 luv v7[0], 464(r1)
 vclr v27
+branch_0x2280:
 vge v7, v7, v31[0w]
+branch_0x2284:
 lpv v5[0], 448(r1)
 vadd v27, v27, v7
 luv v7[0], 432(r1)
@@ -1243,31 +1307,33 @@ vmulf v6, v4, v5
 vadd v3, v6, v6[1q]
 vadd v6, v3, v6[2h]
 vmulf v7, v7, v6[0h]
-bgtz $1, 0x4002280
+bgtz $1, branch_0x2280
 addi $1, $1, 65504
 suv v27[0], 0($7)
 andi $8, $3, 4
 sb $15, 3($7)
 sb $16, 7($7)
 lw $15, 0($7)
-beq $8, $0, 0x4001f48
+beq $8, $0, branch_0x1F48
 lw $16, 4($7)
 andi $8, $3, 8
 lpv v7[0], 144(r29)
 ldv v6[0], 160(r0)
 vmadn v20, v7, v20[0h]
-beq $8, $0, 0x40022f4
+beq $8, $0, branch_0x22F4
 vmadm v18, v31, v31[0w]
 vmulf v7, v18, v18
 vmulf v7, v7, v18
 vmulf v20, v7, v6[1w]
 vmacf v20, v7, v6[3w]
 vmacf v18, v18, v6[2w]
-j 0x4001498
+branch_0x22F4:
+j branch_0x1498
 vadd v18, v18, v31[4w]
+branch_0x22FC:
 andi $1, $1, 4095
 sw $1, 300($0)
-jal 0x4001724
+jal branch_0x1724
 li $8, 992
 ori $8, $0, 3552
 stv v8[2], 16($8)
@@ -1284,6 +1350,7 @@ sdv v14[8], 48($8)
 ldv v12[0], 16(r8)
 ldv v13[0], 32(r8)
 ldv v14[0], 48(r8)
+branch_0x2348:
 lpv v5[0], 440(r1)
 vmulf v5, v5, v31[4w]
 vmudn v6, v12, v5[0h]
@@ -1321,15 +1388,15 @@ vmudn v7, v7, v2[2w]
 spv v7[0], 448($1)
 lw $8, 448($1)
 sw $8, 452($1)
-bgtz $1, 0x4002348
+bgtz $1, branch_0x2348
 addi $1, $1, 65504
-j 0x4001710
+j branch_0x1710
 lh $31, 160($0)
 sll $0, $0, 0
-j 0x40017c8
+j branch_0x17C8
 sll $0, $0, 0
 sll $0, $0, 0
-jal 0x400112c
+jal branch_0x112C
 ori $2, $0, 16384
 mtc0 $2, $4
 break
@@ -1342,11 +1409,11 @@ sw $23, 3056($0)
 lw $19, 264($0)
 move $20, $0
 ori $18, $0, 3071
-jal 0x4001140
+jal branch_0x1140
 ori $17, $0, 1
-jal 0x400112c
+jal branch_0x112C
 sll $0, $0, 0
-j 0x40010bc
+j branch_0x10BC
 mtc0 $2, $4
 sll $0, $0, 0
 sll $0, $0, 0
